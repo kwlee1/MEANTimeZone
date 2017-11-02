7959d468312ae35e45b7216a1730bc56af1dc416
kwlee <keith.won.lee@gmail.com>
2017-11-02 16:04:59 -0700

    started time zone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; //newly added
+import { HttpModule } from '@angular/http'; //newly added
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, //newly added
+    HttpModule //newly added
   ],
   providers: [],
   bootstrap: [AppComponent]
